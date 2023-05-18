648dc2f7002e6bcc2af0209c232cf4c210564fc7
choudharyayushi <97155399+choudharyayushi@users.noreply.github.com>
2023-05-19 00:37:15 +0530

    multi-scrape is left

--- scrapper/views.py
+++ scrapper/views.py
@@ -5,7 +5,7 @@ from django.http import response
 from django.shortcuts import render
 from django.http import JsonResponse
 import pandas as pd 
-
+import re 
 from django.http import HttpResponseRedirect
 from .forms import QueryForm
 from .serializer import UserSerializer
@@ -39,40 +39,82 @@ def loader(request):
 def login(request):
     return render(request, 'login.html')
 
-def get_serp_results(query):
-    API_KEY = 'ab5c8496b40ee68d2b887dc8587cdb62b0f538ad87b479ae1374280242ef084c'
-    endpoint = 'https://serpapi.com/search'
-    params = {
-        'api_key': API_KEY,
-        'q': query,
-        'num': 100,
-        'output': 'json',
-        'hl': 'en'
-    }
-    response = requests.get(endpoint, params=params)
-    data = response.json()
-    search_results = []
-    for result in data['organic_results']:
-        search_result = {}
-        if isinstance(result, dict):
-            search_result['name'] = result.get('title')
-            rich_snippet = result.get('rich_snippet', {}).get('top', {}).get('extensions', {})
-            search_result['rating'] = rich_snippet.get('rating') if isinstance(rich_snippet, dict) else None
-            search_result['address'] = rich_snippet.get('address') if isinstance(rich_snippet, dict) else None
-            search_result['phone'] = rich_snippet.get('phone') if isinstance(rich_snippet, dict) else None
-            search_result['link'] = result.get('links', {}).get('link') if isinstance(rich_snippet, dict) else None
-            search_results.append(search_result)
-            print(search_results)
-    return search_results
+def get_serp_results(request):
+    
+    # Serp API endpoint and parameters
+    api_key = 'ab5c8496b40ee68d2b887dc8587cdb62b0f538ad87b479ae1374280242ef084c'
+    query = request.GET.get('q', '')  # Get the query entered by the user
+    search_engine = 'google_maps'
+    num_results = 10  # Number of results to retrieve
+
+    # Extract the number and location from the query using regular expressions
+    number_match = re.search(r'\b(\d+)\b', query)
+    location_match = re.search(r'in\s+([^,]+)', query)
+
+    number = int(number_match.group(1)) if number_match else num_results
+    location = location_match.group(1).strip() if location_match else ''  # Default location if not specified
+
+    url = f"https://serpapi.com/search.json?engine={search_engine}&q={query}&location={location}&num={number}&api_key={api_key}"
+    print(url)
+    try:
+        response = requests.get(url)
+        data = response.json()
+        map_results = data.get('local_results', [])
+    except requests.RequestException as e:
+        # Handle request exception
+        map_results = []
+    return map_results;
 
 def search(request):
     if request.method == 'GET':
         query = request.GET.get('q')
         if query:
-            results = get_serp_results(query)
+            results = get_serp_results(request)
             context = {'results': results}
             return render(request, 'loader.html', context)
     return render(request, 'index.html')
+
+
+def save_to_excel(request):
+    if request.method == 'GET':
+        query = request.GET.get('q')
+        search_results = get_serp_results(request)
+        
+        # Create a DataFrame from the search results
+        df = pd.DataFrame(search_results)
+        
+        # Select the desired columns
+        columns = ['title', 'website', 'rating', 'address', 'phone']
+        
+        # Save the DataFrame to an Excel file
+        filepath = 'search_results.xlsx'
+        df.to_excel(filepath, index=False)
+        
+        # Prepare the response for file download
+        with open(filepath, 'rb') as file:
+            response = HttpResponse(file.read(), content_type='application/vnd.ms-excel')
+            response['Content-Disposition'] = 'attachment; filename="search_results.xlsx"'
+    
+    return response
+
+
+
+def search_faq(request):
+    # Get search query from GET parameters
+    query = request.GET.get('q')
+
+    if query:
+        # Search for Faq objects that match the query
+        results = Faq.objects.filter(
+            Q(question__icontains=query) | Q(answer__icontains=query)
+        )
+    else:
+        results = None
+
+    # Render the search results in the search_faq.html template
+    return render(request, 'faq.html', {'results': results})
+
+
 
 
 # def search_results(request):
@@ -106,37 +148,9 @@ def search(request):
 #         return render(request, 'index.html')
 
 
-
-def save_to_excel(request):
-    query = request.GET.get('q')
-    search_results = get_serp_results(query)
-    df = pd.DataFrame(search_results)
-    df.to_excel('search_results.xlsx', index=False)
-    response = HttpResponse(content_type='application/vnd.ms-excel')
-    response['Content-Disposition'] = 'attachment; filename="search_results.xlsx"'
-    df.to_excel(response, index=False)
-    return response
-
 # def save_to_excel(request):
 #     file_path = 'results.xlsx'
 #     with open(file_path, 'rb') as fh:
 #         response = HttpResponse(fh.read(), content_type='application/vnd.ms-excel')
 #         response['Content-Disposition'] = 'attachment; filename=' + file_path.split('/')[-1]
 #         return response
-    
-    
-    
-def search_faq(request):
-    # Get search query from GET parameters
-    query = request.GET.get('q')
-
-    if query:
-        # Search for Faq objects that match the query
-        results = Faq.objects.filter(
-            Q(question__icontains=query) | Q(answer__icontains=query)
-        )
-    else:
-        results = None
-
-    # Render the search results in the search_faq.html template
-    return render(request, 'faq.html', {'results': results})
